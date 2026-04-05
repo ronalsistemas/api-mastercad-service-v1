aa5a2585e55e57ce4d4f87adb6e73d764e0d6ee5
Ronal Casani <ronalsistemas@gmail.com>
2026-04-05 16:01:07 -0500

    ms base api-mastercad-service-v1

---
+++ src/main/java/com/rcasani/ApiMastercadServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.rcasani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMastercadServiceV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMastercadServiceV1Application.class, args);
+    }
+
+}
